07503ab1d2469a1cfb217bfced0b279d322f6ee7
MrAnger <slava.sharapov@mail.ru>
2017-02-22 16:31:55 +0700

    Правка простановки IP адреса регистрации

--- frontend/components/StartUp.php
+++ frontend/components/StartUp.php
@@ -17,26 +17,6 @@ class StartUp implements BootstrapInterface {
 	 * @inheritDoc
 	 */
 	public function bootstrap($app) {
-		// После создания нового профиля пользователя, проверяем настроен ли емайл gravatar, если нет, то устанавливаем его принудительно
-		Event::on(Profile::className(), Profile::EVENT_AFTER_INSERT, function (AfterSaveEvent $event) {
-			/** @var Profile $profile */
-			$profile = $event->sender;
-
-			if ($profile->gravatar_email === null) {
-				$profile->gravatar_email = $profile->user->email;
-
-				$profile->save(false);
-			}
-		});
-
-		// Прописываем правильный IP адрес при создании пользователя
-		Event::on(User::className(), User::AFTER_CREATE, function (Event $event) {
-			/** @var User $user */
-			$user = $event->sender;
-
-			if (isset($_SERVER['HTTP_X_REAL_IP'])) {
-				$user->updateAttributes(['registration_ip' => $_SERVER['HTTP_X_REAL_IP']]);
-			}
-		});
+		
 	}
 }
